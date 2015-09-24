f7a1690e1c952752836e0e43af1dea19fcb52f5b
parrt <parrt@cs.usfca.edu>
2015-09-24 14:58:56 -0700

    fix unit test output. test currently failing.

--- trans/test/t14_expected.c
+++ trans/test/t14_expected.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 #include "wich.h"
 
+int x;
+
 void f();
 
 void f()
 {
-	Vector *x = Vector_new((double []){1,2,3}, 3);
-	REF(x);
-	DEREF(x);
-	return x;
+	g();
 }
 
 int main(int argc, char *argv[])
 {
-	Vector *tmp1;
-	Vector *tmp2;
-	Vector *tmp3;
-	print_vector(tmp3=Vector_add(tmp1=f(),tmp2=f()));
-	DEREF(tmp1);
-	DEREF(tmp2);
-	DEREF(tmp3);
+    x = 99;
 	return 0;
 }
